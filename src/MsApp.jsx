48cc2894fb8a57d30bac463145240945f0262d43
charley43613 <41710701+charley43613@users.noreply.github.com>
2023-03-10 16:20:47 -0500

    Update MsApp.jsx

--- src/MsApp.jsx
+++ src/MsApp.jsx
@@ -8,7 +8,8 @@ const MsApp = () => {
 
     const doFetch = async () => {
 //         let result = await fetch('/api/sample/')
-        let result = await fetch('https://gateway-1-charles3.apps.qqd02ebq.usgovvirginia.aroapp.azure.us/sample/')
+        let result = await fetch('https://duplciate-fe-front-end-new.apps.qqd02ebq.usgovvirginia.aroapp.azure.us/api/sample/')
+//         let result = await fetch('https://gateway-1-charles3.apps.qqd02ebq.usgovvirginia.aroapp.azure.us/sample/')
         result = await result.text()
         setMessage(callMessage)
         setData(result)  
